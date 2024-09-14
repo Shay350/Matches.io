19b5298e197ed0984c6eb645670a7ff286e9b49f
sameersul <sameerjuin@gmail.com>
2024-09-14 12:51:22 -0400

    fixed header mishaps, added foundation

--- my-app/src/components/Navbar.js
+++ my-app/src/components/Navbar.js
@@ -2,12 +2,14 @@ import React from 'react';
 
 const Navbar = () => {
   return (
-    <nav className="flex justify-between items-center bg-navy-blue p-4">
-      <div className="text-white text-xl font-bold">CareersIO</div>
-      <button className="bg-pink-500 hover:bg-pink-600 text-white font-bold py-2 px-4 rounded">
-        Submit
-      </button>
-    </nav>
+    <div className="flex justify-center w-full bg-black">
+      <nav className="flex justify-between items-center w-4/5 p-4">
+        <div className="text-white text-xl font-bold">CareersIO</div>
+        <button className="bg-pink-500 hover:bg-pink-600 text-white font-bold py-2 px-4 rounded">
+          Submit
+        </button>
+      </nav>
+    </div>
   );
 };
 
