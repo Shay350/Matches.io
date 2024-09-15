a13e8c9723c0a6b9c0e125ffa57cb36710e252f0
sameersul <sameerjuin@gmail.com>
2024-09-15 00:48:35 -0400

    updated navbar

--- my-app/src/components/Navbar.js
+++ my-app/src/components/Navbar.js
@@ -1,39 +1,41 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import logo from './logo.png'
+import logo from './logo.png'; // Ensure this path is correct
 
 const Navbar = () => {
   return (
-    <div className="flex justify-center w-full bg-blue-900 shadow-lg">
-      <nav className="flex justify-between items-center w-4/5 p-4">
-        
-
-        <div className="flex space-x-4">
-
+    <div className="navbar-container">
+      <nav className="flex justify-between items-center w-full p-4">
+        {/* Left side: Logo */}
+        <div className="flex items-center">
           <a href="http://localhost:3000/">
-            <img id="Logo" src={logo} alt="CareersIO Logo" className="h-0" />
+            <img id="Logo" src={logo} alt="CareersIO Logo" className="h-12" />
           </a>
+        </div>
 
-          <Link to="/job-listings" className="text-white font-bold py-2 px-4">
+        {/* Center: Links */}
+        <div className="flex space-x-8 items-center">
+          <Link to="/job-listings" className="navbar-link">
             About Us
           </Link>
-
-          <Link to="/job-options" className="text-white font-bold py-2 px-4">
-            Job Options
+          <Link to="/job-options" className="navbar-link">
+            How It Works
           </Link>
-          
-          <Link to="/resume-upload" className="text-white font-bold py-2 px-4">
-            Resume Upload
+          <Link to="/resume-upload" className="navbar-link">
+            Why
           </Link>
-
-          <Link to="/job-listings" className="text-white font-bold py-2 px-4">
+          {/* <Link to="/job-listings" className="navbar-link">
             Job Listings
-          </Link>
+          </Link> */}
+        </div>
 
-          <button id="Upload" class="button">
+        {/* Right side: Button */}
+        <div className="flex items-center">
+          <button className="navbar-button">
+            <Link to="/resume-upload" className="navbar-link">
             Upload Resume
+          </Link>
           </button>
-
         </div>
       </nav>
     </div>
