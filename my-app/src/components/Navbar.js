fb655d8a1b44c8fc9b4350aad284f3f12a09975f
lukeqin-s <lukeqins@gmail.com>
2024-09-14 23:51:10 -0400

    Update Navbar.js
    changed button design

--- my-app/src/components/Navbar.js
+++ my-app/src/components/Navbar.js
@@ -1,27 +1,39 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import logo from './logo.png'
 
 const Navbar = () => {
   return (
     <div className="flex justify-center w-full bg-blue-900 shadow-lg">
       <nav className="flex justify-between items-center w-4/5 p-4">
-        <div className="text-white text-xl font-bold">CareersIO</div>
+        
+
         <div className="flex space-x-4">
-          <Link to="/" className="text-white font-bold py-2 px-4">
-            Home
+
+          <a href="http://localhost:3000/">
+            <img id="Logo" src={logo} alt="CareersIO Logo" className="h-0" />
+          </a>
+
+          <Link to="/job-listings" className="text-white font-bold py-2 px-4">
+            About Us
           </Link>
+
           <Link to="/job-options" className="text-white font-bold py-2 px-4">
             Job Options
           </Link>
+          
           <Link to="/resume-upload" className="text-white font-bold py-2 px-4">
             Resume Upload
           </Link>
+
           <Link to="/job-listings" className="text-white font-bold py-2 px-4">
             Job Listings
           </Link>
-          <button className="bg-pink-500 hover:bg-pink-600 transition-colors duration-300 text-white font-bold py-2 px-4 rounded">
-            Submit
+
+          <button id="Upload" class="button">
+            Upload Resume
           </button>
+
         </div>
       </nav>
     </div>
